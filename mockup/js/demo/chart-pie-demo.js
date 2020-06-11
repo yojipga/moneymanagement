4d59c2c66ec3e2e85d2a8ebf8201d5ec60684d38
Yoji Matsuyama <me@yojimatsuyama.com>
2020-06-11 13:39:11 +0700

    added mockup

---
+++ mockup/js/demo/chart-pie-demo.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["普通預金", "定期預金", "株式", "その他"],
+    datasets: [{
+      data: [64.8, 8.1, 19.9, 7.2],
+      backgroundColor: ['#e74a3b', '#f6c23e', '#4e73df', '#36b9cc'],
+      hoverBackgroundColor: ['#e74a3b', '#f6c23e', '#4e73df', '#36b9cc'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
